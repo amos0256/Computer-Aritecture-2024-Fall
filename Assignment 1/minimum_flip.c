e2ecb24624640ef5b1cd7377d78d8785a5e95a63
amos50308@gmail.com <amos50308@gmail.com>
2024-10-05 19:50:42 +0800

    [mod] revise input parameters and format

--- Assignment 1/minimum_flip.c
+++ Assignment 1/minimum_flip.c
@@ -1,5 +1,6 @@
 /* leetcode 1318 Minimum Flips to Make a OR b Equal to c */
 /* https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/description/?envType=problem-list-v2&envId=bit-manipulation */
+#include <stdio.h>
 
 int minFlips(int a, int b, int c){
   int flips = 0;
@@ -20,4 +21,21 @@ int minFlips(int a, int b, int c){
   }
 
   return flips;
+}
+
+int main(void) {
+  // examples
+  int a[3] = {143165576, 51041, 1};
+  int b[3] = {715827882, 65280, 2};
+  int c[3] = {1, 716177407, 3};
+  // expected output:
+  // example 1: 23
+  // example 2: 14
+  // example 3: 0
+
+  for (int i = 0; i < 3; i++) {
+    printf("Minimum flips is %d.\n", minFlips(a[i], b[i], c[i]));
+  }
+
+  return 0;
 }
--- Assignment 1/minimum_flip.s
+++ Assignment 1/minimum_flip.s
@@ -1,8 +1,8 @@
     .data
 example_1:
-    .word 2, 6, 5
+    .word 143165576, 715827882, 1
 example_2:
-    .word 4, 2, 7
+    .word 51041, 65280, 716177407
 example_3:
     .word 1, 2, 3
 format:
@@ -14,7 +14,7 @@ newline:
     .global main
 main:
     # example_1
-    la    t0, example_1        # load address of example_1
+    la    t0, example_1       # load address of example_1
     lw    a0, 0(t0)
     lw    a1, 4(t0)
     lw    a2, 8(t0)
@@ -22,7 +22,7 @@ main:
     jal   ra, print_result
 
     # example_2
-    la    t0, example_2        # load address of example_2
+    la    t0, example_2       # load address of example_2
     lw    a0, 0(t0)
     lw    a1, 4(t0)
     lw    a2, 8(t0)
@@ -30,7 +30,7 @@ main:
     jal   ra, print_result
 
     # example_3
-    la    t0, example_3        # load address of example_3
+    la    t0, example_3       # load address of example_3
     lw    a0, 0(t0)
     lw    a1, 4(t0)
     lw    a2, 8(t0)
@@ -38,61 +38,61 @@ main:
     jal   ra, print_result
 
     # exit program
-    li    a7, 10               # syscall exit
+    li    a7, 10              # syscall exit
     ecall
 
 # min flips function
 min_flips:
-    li    t0, 0                # flips = 0
-    li    t1, 31               # i = 31, 31 bits
+    li    t0, 0               # flips = 0
+    li    t1, 31              # i = 31, 31 bits
 
 loop:
-    bltz   t1, end_loop        # if i < 0, goto end_loop
+    bltz  t1, end_loop        # if i < 0, goto end_loop
 
-    srl    t2, a0, t1          # bitA = (a >> i) & 1
-    andi   t2, t2, 1
+    srl   t2, a0, t1          # bitA = (a >> i) & 1
+    andi  t2, t2, 1
 
-    srl    t3, a1, t1          # bitB = (b >> i) & 1
-    andi   t3, t3, 1
+    srl   t3, a1, t1          # bitB = (b >> i) & 1
+    andi  t3, t3, 1
 
-    srl    t4, a2, t1          # bitC = (c >> i) & 1
-    andi   t4, t4, 1
+    srl   t4, a2, t1          # bitC = (c >> i) & 1
+    andi  t4, t4, 1
 
-    beqz   t4, add_flip        # if bitC == 0, goto add_flip
-    beqz   t2, check_b         # if bitA == 0, goto check_b
-    j      continue_loop
+    beqz  t4, add_flip        # if bitC == 0, goto add_flip
+    beqz  t2, check_b         # if bitA == 0, goto check_b
+    j     continue_loop
 
 add_flip:
-    add    t0, t0, t2          # flips += bitA
-    add    t0, t0, t3          # flips += bitB
-    j      continue_loop
+    add   t0, t0, t2          # flips += bitA
+    add   t0, t0, t3          # flips += bitB
+    j     continue_loop
 
 check_b:
-    bnez   t3, continue_loop   # if bitB != 0, goto continue_loop
-    addi   t0, t0, 1           # flips += 1
+    bnez  t3, continue_loop   # if bitB != 0, goto continue_loop
+    addi  t0, t0, 1           # flips += 1
 
 continue_loop:
-    addi   t1, t1, -1          # i--
-    j      loop
+    addi  t1, t1, -1          # i--
+    j     loop
 
 end_loop:
-    mv     a0, t0              # restore return value
+    mv    a0, t0              # restore return value
     ret
 
 # print result function
 print_result:
-    mv t0, a0                  # t0 = minimum flips result
+    mv    t0, a0              # t0 = minimum flips result
     
-    la a0, format
-    li a7, 4
+    la    a0, format
+    li    a7, 4
     ecall
 
-    mv a0, t0
-    li a7, 1
+    mv    a0, t0
+    li    a7, 1
     ecall
 
-    la a0, newline
-    li a7, 4
+    la    a0, newline
+    li    a7, 4
     ecall
 
     ret
