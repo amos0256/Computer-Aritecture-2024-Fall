8f3ffbd44f1ae51dc7efeebbbd89260c6e4a25d6
amos50308@gmail.com <amos50308@gmail.com>
2024-10-09 20:03:31 +0800

    [mod] format modify

--- Assignment 1/minimum_flip_clz.s
+++ Assignment 1/minimum_flip_clz.s
@@ -5,6 +5,7 @@ example_2:
     .word 51041, 65280, 716177407
 example_3:
     .word 143165576, 715827882, 1
+
 format:
     .asciz "Minimum flips is "
 newline:
@@ -100,33 +101,33 @@ min_flips:
     addi  sp, sp, 12          # deallocate space from the stack
 
 loop:
-    bltz   t1, end_loop       # if i < 0, goto end_loop
+    bltz  t1, end_loop        # if i < 0, goto end_loop
 
-    srl    t2, a0, t1         # bitA = (a >> i) & 1
-    andi   t2, t2, 1
+    srl   t2, a0, t1          # bitA = (a >> i) & 1
+    andi  t2, t2, 1
 
-    srl    t3, a1, t1         # bitB = (b >> i) & 1
-    andi   t3, t3, 1
+    srl   t3, a1, t1          # bitB = (b >> i) & 1
+    andi  t3, t3, 1
 
-    srl    t4, a2, t1         # bitC = (c >> i) & 1
-    andi   t4, t4, 1
+    srl   t4, a2, t1          # bitC = (c >> i) & 1
+    andi  t4, t4, 1
 
-    beqz   t4, add_flip       # if bitC == 0, goto add_flip
-    beqz   t2, check_b        # if bitA == 0, goto check_b
-    j      continue_loop
+    beqz  t4, add_flip        # if bitC == 0, goto add_flip
+    beqz  t2, check_b         # if bitA == 0, goto check_b
+    j     continue_loop
 
 add_flip:
-    add    t0, t0, t2         # flips += bitA
-    add    t0, t0, t3         # flips += bitB
-    j      continue_loop
+    add   t0, t0, t2          # flips += bitA
+    add   t0, t0, t3          # flips += bitB
+    j     continue_loop
 
 check_b:
-    bnez   t3, continue_loop  # if bitB != 0, goto continue_loop
-    addi   t0, t0, 1          # flips += 1
+    bnez  t3, continue_loop   # if bitB != 0, goto continue_loop
+    addi  t0, t0, 1           # flips += 1
 
 continue_loop:
-    addi   t1, t1, -1         # i--
-    j      loop
+    addi  t1, t1, -1          # i--
+    j     loop
 
 end_loop:
     mv     a0, t0             # restore return value
